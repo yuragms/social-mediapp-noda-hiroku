00f59a6313cc532d852be4d1533ada12515e1e9a
yuragms <yura.gms@gmail.com>
2023-03-24 13:34:40 +0200

    1

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import cors from "cors";
-import AuthRoute from "./Routes/AuthRoute.js";
-import UserRoute from "./Routes/UserRoute.js";
-import PostRoute from "./Routes/PostRoute.js";
-import UploadRoute from "./Routes/UploadRoute.js";
-import ChatRoute from "./Routes/ChatRoute.js";
-import MessageRoute from "./Routes/MessageRoute.js";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import AuthRoute from './Routes/AuthRoute.js';
+import UserRoute from './Routes/UserRoute.js';
+import PostRoute from './Routes/PostRoute.js';
+import UploadRoute from './Routes/UploadRoute.js';
+import ChatRoute from './Routes/ChatRoute.js';
+import MessageRoute from './Routes/MessageRoute.js';
 
 // Routes
 
 const app = express();
 
 //to serve images for public
-app.use(express.static("public"));
-app.use("/images", express.static("images"));
+app.use(express.static('public'));
+app.use('/images', express.static('images'));
 
 // Middleware
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors({ origin: '*' }));
 
 dotenv.config();
 
@@ -40,9 +40,9 @@ mongoose
 
 // usage of routes
 
-app.use("/auth", AuthRoute);
-app.use("/user", UserRoute);
-app.use("/posts", PostRoute);
-app.use("/upload", UploadRoute);
-app.use("/chat", ChatRoute);
-app.use("/messages", MessageRoute);
+app.use('/auth', AuthRoute);
+app.use('/user', UserRoute);
+app.use('/posts', PostRoute);
+app.use('/upload', UploadRoute);
+app.use('/chat', ChatRoute);
+app.use('/messages', MessageRoute);
